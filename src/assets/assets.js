d83d8b08c866d325ebb3636afcf3530e3307ef5d
sam-cant-code <135305742+sam-cant-code@users.noreply.github.com>
2025-06-13 22:31:25 +0530

    added menu items

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,42 +7,28 @@ import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
 import menu_4 from './menu_4.png'
 import menu_5 from './menu_5.png'
-// import menu_6 from './menu_6.png'
-// import menu_7 from './menu_7.png'
-// import menu_8 from './menu_8.png'
 
-import food_1 from './food_1.png'
-import food_2 from './food_2.png'
-import food_3 from './food_3.png'
-import food_4 from './food_4.png'
-import food_5 from './food_5.png'
-import food_6 from './food_6.png'
-import food_7 from './food_7.png'
-import food_8 from './food_8.png'
-import food_9 from './food_9.png'
-import food_10 from './food_10.png'
-import food_11 from './food_11.png'
-import food_12 from './food_12.png'
-import food_13 from './food_13.png'
-import food_14 from './food_14.png'
-import food_15 from './food_15.png'
-import food_16 from './food_16.png'
-import food_17 from './food_17.png'
-import food_18 from './food_18.png'
-import food_19 from './food_19.png'
-import food_20 from './food_20.png'
-import food_21 from './food_21.png'
-import food_22 from './food_22.png'
-import food_23 from './food_23.png'
-import food_24 from './food_24.png'
-import food_25 from './food_25.png'
-import food_26 from './food_26.png'
-import food_27 from './food_27.png'
-import food_28 from './food_28.png'
-import food_29 from './food_29.png'
-import food_30 from './food_30.png'
-import food_31 from './food_31.png'
-import food_32 from './food_32.png'
+
+import brownie_classic from './brownie_classic.png'
+import brownie_ragi from './brownie_ragi.png'
+import brownie_nuts from './brownie_nuts.png'
+import brownie_whitechocolate from './brownie_whitechocolate.png'
+import brownie_rosemilk from './brownie_rosemilk.png'
+import cakes_teacake from './cakes_teacake.png'
+import cakes_plumcake from './cakes_plumcake.png'
+import cakes_datecake from './cakes_datecake.png'
+import friedtreats_doughnut from './friedtreats_doughnut.png'
+import friedtreats_bombolini from './friedtreats_bombolini.png'
+import cookies from './cookies1.png'
+import muttamittai from './specialitysweets_muttamittai.png'
+
+
+
+
+
+
+
+
 
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
@@ -108,232 +94,100 @@ export const menu_list = [
 export const food_list = [
     {
         _id: "1",
-        name: "Greek salad",
-        image: food_1,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
+        name: "Classic Fudgy Brownie",
+        image: brownie_classic, 
+        price: 350,
+        description: "Rich and fudgy classic brownie (1/2 kg)",
+        category: "Brownies"
     },
     {
         _id: "2",
-        name: "Veg salad",
-        image: food_2,
-        price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
-    }, {
+        name: "Ragi Brownie",
+        image: brownie_ragi,
+        price: 375,
+        description: "Healthy ragi brownie with rich taste (1/2 kg)",
+        category: "Brownies"
+    },
+    {
         _id: "3",
-        name: "Clover Salad",
-        image: food_3,
-        price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
-    }, {
+        name: "Classic Brownie with Almonds & Chocolate Filling",
+        image: brownie_nuts,
+        price: 400,
+        description: "Classic brownie loaded with almonds and chocolate filling (1/2 kg)",
+        category: "Brownies"
+    },
+    {
         _id: "4",
-        name: "Chicken Salad",
-        image: food_4,
-        price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
-    }, {
+        name: "White Chocolate Brownie",
+        image: brownie_whitechocolate,
+        price: 400,
+        description: "Decadent white chocolate brownie (1/2 kg)",
+        category: "Brownies"
+    },
+    ,
+    {
         _id: "5",
-        name: "Lasagna Rolls",
-        image: food_5,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
+        name: "Rose Milk Brownie",
+        image: brownie_rosemilk,
+        price: 375,
+        description: "Unique rose milk flavored brownie",
+        category: "Brownies"
+    },
+    {
         _id: "6",
-        name: "Peri Peri Rolls",
-        image: food_6,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
+        name: "Tea Cake",
+        image: cakes_teacake,
+        price: 275,
+        description: "Classic tea cake perfect for evening snacks (1/2 kg)",
+        category: "Cakes"
+    },
+    {
         _id: "7",
-        name: "Chicken Rolls",
-        image: food_7,
-        price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
+        name: "Rich Plum Cake",
+        image: cakes_plumcake,
+        price: 450,
+        description: "Traditional rich plum cake with dry fruits",
+        category: "Cakes"
+    },
+    {
         _id: "8",
-        name: "Veg Rolls",
-        image: food_8,
-        price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
+        name: "Dates Cake",
+        image: cakes_datecake,
+        price: 400,
+        description: "Healthy and delicious dates cake",
+        category: "Cakes"
+    },
+    {
         _id: "9",
-        name: "Ripple Ice Cream",
-        image: food_9,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
-    }, {
+        name: "Doughnuts",
+        image: friedtreats_doughnut,
+        price: 50,
+        description: "Fresh and fluffy doughnuts",
+        category: "Fried Treats"
+    },
+    {
         _id: "10",
-        name: "Fruit Ice Cream",
-        image: food_10,
-        price: 22,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
-    }, {
+        name: "Bombaloni",
+        image: friedtreats_bombolini,
+        price: 60,
+        description: "Italian-style filled doughnuts",
+        category: "Fried Treats"
+    },
+    {
         _id: "11",
-        name: "Jar Ice Cream",
-        image: food_11,
-        price: 10,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
-    }, {
+        name: "Cookies", 
+        image: cookies,
+        price: 25,
+        description: "Freshly baked cookies",
+        category: "Cookies"
+    },
+    {
         _id: "12",
-        name: "Vanilla Ice Cream",
-        image: food_12,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
+        name: "Mutta Mittai",
+        image: muttamittai,
+        price: 40,
+        description: "Traditional egg-shaped sweet",
+        category: "Speciality Sweets"
     },
-    {
-        _id: "13",
-        name: "Chicken Sandwich",
-        image: food_13,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    },
-    {
-        _id: "14",
-        name: "Vegan Sandwich",
-        image: food_14,
-        price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    }, {
-        _id: "15",
-        name: "Grilled Sandwich",
-        image: food_15,
-        price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    }, {
-        _id: "16",
-        name: "Bread Sandwich",
-        image: food_16,
-        price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    }, {
-        _id: "17",
-        name: "Cup Cake",
-        image: food_17,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "18",
-        name: "Vegan Cake",
-        image: food_18,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "19",
-        name: "Butterscotch Cake",
-        image: food_19,
-        price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "20",
-        name: "Sliced Cake",
-        image: food_20,
-        price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "21",
-        name: "Garlic Mushroom ",
-        image: food_21,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    }, {
-        _id: "22",
-        name: "Fried Cauliflower",
-        image: food_22,
-        price: 22,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    }, {
-        _id: "23",
-        name: "Mix Veg Pulao",
-        image: food_23,
-        price: 10,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    }, {
-        _id: "24",
-        name: "Rice Zucchini",
-        image: food_24,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    },
-    {
-        _id: "25",
-        name: "Cheese Pasta",
-        image: food_25,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    },
-    {
-        _id: "26",
-        name: "Tomato Pasta",
-        image: food_26,
-        price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    }, {
-        _id: "27",
-        name: "Creamy Pasta",
-        image: food_27,
-        price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    }, {
-        _id: "28",
-        name: "Chicken Pasta",
-        image: food_28,
-        price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    }, {
-        _id: "29",
-        name: "Buttter Noodles",
-        image: food_29,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }, {
-        _id: "30",
-        name: "Veg Noodles",
-        image: food_30,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }, {
-        _id: "31",
-        name: "Somen Noodles",
-        image: food_31,
-        price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }, {
-        _id: "32",
-        name: "Cooked Noodles",
-        image: food_32,
-        price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }
+
 ]
---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import {StoreContext} from '../../context/StoreContext'
+
+const FoodDisplay = ({category}) => {
+
+    const {food_list} = useContext(StoreContext)
+
+  return (
+    <div className="food-display" id='food-display'>
+      <h2>Pastry List</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index)=>{
+            return(
+              <div className="food-item">
+                <div className="food-item-image-container">
+                  <img className="food-item-image" src={item.image} alt="" />
+                </div>
+                <div className="food-item-info">
+                  <div className="food-item-name">
+                    <p>{item.name}</p>
+                  </div>
+                  <p className="food-item-description">{item.description}</p>
+                  <p className="food-item-price">₹{item.price}</p>
+                </div>
+              </div>
+            )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
