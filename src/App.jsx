95ffd12f27e2c3ef9bee928e5c91d45632c1b331
sam-cant-code <135305742+sam-cant-code@users.noreply.github.com>
2025-06-14 15:58:01 +0530

    deploybug fix hopefully

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Route } from 'react-router'
-import Home from './pages/Home/home'
+import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
